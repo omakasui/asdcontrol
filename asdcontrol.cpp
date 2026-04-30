46fe08bf8251f37799c2833b0b194245530531a2
Luca Pattocchio <15154851+Kasui92@users.noreply.github.com>
2026-04-30 10:17:47 +0200

    Bump version to 0.6.0

--- asdcontrol.cpp
+++ asdcontrol.cpp
@@ -42,7 +42,7 @@ using namespace std;
 #define HID_MAX_APPLICATIONS	16
 
 // Current version
-#define VERSION "0.5"
+#define VERSION "0.6.0"
 
 // Operation modes for this software
 const int USAGE_MODE_GET = 0;
